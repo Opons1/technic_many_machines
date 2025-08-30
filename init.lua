852312b09af9de7d98acb56960d5d5c519ffee75
Opons1 <98196027+Opons1@users.noreply.github.com>
2025-08-30 16:51:03 -0700

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,27 @@
+technic.register_recipe_type("stone_fabricator", {description = "Stone Fabricating"})
+
+core.register_node("technic_many_machines:stone_fabricator", {
+    description = "MV Stone Fabricator",
+    tiles = {"default_steel_block.png"},
+    groups = {cracky = 1, technic_machine = 1, technic_mv = 1},
+    sounds = default.node_sound_metal_defaults(),
+})
+technic.register_base_machine("technic_many_machines:stone_fabricator", {
+    tier = "MV",
+    typename = "stone_fabricator",
+    description = "MV Stone Fabricator",
+    demand = {2000, 1500, 1000},
+    speed = 1,
+    upgrade = 1,
+    tube = 1,
+})
+-- Register recipe
+technic.register_recipe("stone_fabricator", {
+    input = {
+        "default:coal_lump",
+        "default:gold_ingot",
+        "default:coalblock",
+    },
+    output = "default:diamond",
+    time = 5,
+})

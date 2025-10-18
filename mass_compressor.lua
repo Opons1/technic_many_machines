d418a403b104dba1f55a05572d0294d436ff4a91
Opons1 <tazimc1001@gmail.com>
2025-10-17 20:23:57 -0700

    PROGRESS

--- mass_compressor.lua
+++ mass_compressor.lua
@@ -7,15 +7,26 @@ function technic.register_mass_compressor(data)
 	technic.register_base_machine(data)
 end
 --we be mass compressing now
-technic.register_recipe_type("mass_compressing", {description = "Mass Compressing"})
-technic.register_recipe_type("mass_compressing", {
-	description = ("Mass Compressing"),
-	input_size = 2,
-})
+technic.register_recipe_type("mass_compressing", {description = "Mass Compressing", input_size = 2})
 technic.register_mass_compressor({tier = "HV", demand = {1600, 1200, 800}, speed = 4, upgrade = 1, tube = 1})
 core.register_craftitem("technic_many_machines:compressor_piston", {
     description = "Compressor Piston",
     inventory_image = "technic_many_machines_compressor_piston.png",
+})
+core.override_item("technic_many_machines:hv_masscompressor_active", {
+    tiles = {
+        "technic_many_machines_hv_masscompressor_top.png",
+        "technic_many_machines_hv_masscompressor_bottom.png",
+        "technic_many_machines_hv_masscompressor_side.png",
+        "technic_many_machines_hv_masscompressor_side.png",
+        "technic_many_machines_hv_masscompressor_side.png",
+         {name = "technic_many_machines_hv_masscompressor_front_active.png",
+        animation = {
+            type = "vertical_frames",
+            aspect_w = 16,
+            aspect_h = 16,
+            length = 1, }, },
+        },
 })
 core.register_craft({
     output = "technic_many_machines:compressor_piston",
@@ -32,4 +43,13 @@ core.register_craft({
         {"technic_many_machines:compressor_piston", "technic:hv_transformer", "technic_many_machines:compressor_piston"},
         {"technic:carbon_plate", "technic:hv_cable", "technic:carbon_plate"},
     }
+})
+function technic.register_mass_compressor_recipe(data)
+    data.time = data.time or 4
+    technic.register_recipe("mass_compressing", data)
+end
+technic.register_mass_compressor_recipe({
+    input = {"default:stone", "default:stone"},
+    output = "technic:composite_plate",
+    time = 2,
 })

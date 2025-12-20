390b973cf1ea46af910a75fc0238d81e0213cc14
Opons1 <tazimc1001@gmail.com>
2025-12-20 10:42:09 -0800

    new

---
+++ nodes.lua
@@ -0,0 +1,12 @@
+core.register_node("technic_many_machines:hv_machine_frame", {
+    description = "HV Machine Frame",
+    tiles = {"technic_many_machines_hv_frame.png",
+        "technic_many_machines_hv_frame.png",
+        "technic_many_machines_hv_frame.png",
+        "technic_many_machines_hv_frame.png",
+        "technic_many_machines_hv_frame.png",
+        "technic_many_machines_hv_frame.png"},
+    is_ground_content = false,
+    groups = {cracky = 1, level = 2},
+    sounds = default.node_sound_metal_defaults(),
+})

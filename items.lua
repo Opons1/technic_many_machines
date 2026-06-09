837ebdab40ec92d0484c3aa36a4aa7eabe80b188
Opons1 <tazimc1001@gmail.com>
2026-06-08 17:45:06 -0700

    add post effect color and remove visibility of textyre being used for coloring

--- items.lua
+++ items.lua
@@ -124,17 +124,22 @@ register_multiblock_part("hv_mb_airlock", {
     use_texture_alpha = true,
     walkable = false,
     drawtype = "glasslike_framed",
-    tiles = {"technic_many_machines_multiblock_hv_airlock.png", "technic_many_machines_damaged_hv_frame.png^[colorize:#585858:200^[opacity:117"},
+    tiles = {"technic_many_machines_multiblock_hv_airlock.png", "technic_many_machines_damaged_hv_frame.png^[colorize:#585858:255^[opacity:117"},
     groups = {cracky = 1, level = 2},
+    post_effect_color = {r = 88, b = 88, g = 88, a = 117}
+
 })
 register_multiblock_part("hv_mb_glass", {
     description = "HV Glass Part",
     use_texture_alpha = true,
     paramtype = "light",
     drawtype = "glasslike_framed",
-    tiles = {"technic_many_machines_multiblock_hv_glass.png", "technic_many_machines_damaged_hv_frame.png^[opacity:117"},
+    tiles = {"technic_many_machines_multiblock_hv_glass.png", "technic_many_machines_damaged_hv_frame.png^[colorize:#3a3b2a:255^[opacity:117"},
     groups = {cracky = 1, level = 2},
+    post_effect_color = {r = 58, g = 59, b = 42, a = 117 }
+
 })
+
 register_multiblock_part("hv_mb_casing", {
     description = "HV Machine Casing Part",
     tiles = {"technic_many_machines_damaged_hv_frame.png"},

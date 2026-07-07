c497ded5fb26016b8fd6faf400a13cd110ed47fb
Opons1 <tazimc1001@gmail.com>
2026-07-06 18:57:18 -0700

    remove duplicate code

--- machines/mass_compressor.lua
+++ machines/mass_compressor.lua
@@ -38,25 +38,6 @@ function technic_many_machines.register_bulk_compressor_recipe(data)
     technic.register_recipe("bulk_compressing", data)
 end
 
---not wanting to type a lot, also you might not wanna replace you compressor with this, this thing might be too heavy duty for some things
-local recipes = {
-    { 2, "default:copper_ingot 10", "default:copper_ingot 10", "technic:copper_plate 4"},
-    { 2, "default:sand 20", "default:sand 20", "default:sandstone 20"},
-    { 2, "default:desert_sand 20", "default:desert_sand 20", "default:desert_sandstone 20"},
-    { 2, "default:silver_sand 20", "default:silver_sand 20", "default:silver_sandstone 20"},
-    { 2, "technic:mixed_metal_ingot 2", "technic:mixed_metal_ingot 2", "technic:composite_plate 4"},
-    { 2, "technic:coal_dust 24", "technic:coal_dust 24", "technic:carbon_plate 4"},
-	{ 10,"default:coal_lump 25", "default:coal_lump 25", "default:diamond"},
-    { 4, "technic_many_machines:radiant_alloy_ingot 10", "technic_many_machines:radiant_alloy_ingot 10", "technic_many_machines:radiant_alloy_plate 4"},
-    { 2, "technic_many_machines:radiant_alloy_plate", "technic:blast_resistant_concrete 2", "technic_many_machines:hv_mb_casing"},
-    { 2, "technic_many_machines:hv_mb_casing", "default:obsidian_glass 4", "technic_many_machines:hv_mb_glass"}
-}
-for _, recipe in ipairs(recipes) do
-    technic_many_machines.register_bulk_compressor_recipe({
-        time = recipe[1],
-        input = {recipe[2], recipe[3]},
-        output = recipe[4]})
-end
 if core.get_modpath("techage") then
     techage.register_node({"technic_many_machines:hv_masscompressor", "technic_many_machines:hv_masscompressor_active"}, {
         on_push_item = function(pos, in_dir, stack)
